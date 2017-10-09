4033ece40f0a5c813eaa73f5b26b9119bea9c247
Andrea <andrea.rizzi@cern.ch>
2017-10-09 16:18:42 +0200

    test area for vhbb

---
+++ python/postprocessing/analysis/higgs/vhbb/VHbbProducer.py
@@ -0,0 +1,30 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+class VHbbProducer(Module):
+    def __init__(self):
+        pass
+    def beginJob(self):
+        pass
+    def endJob(self):
+        pass
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("Vtype",  "I");
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+        electrons = Collection(event, "Electron")
+        muons = Collection(event, "Muon")
+        jets = Collection(event, "Jet")
+        return True
+
+
+# define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
+
+vhbb = lambda : VHbbProducer()
+ 
---
+++ python/postprocessing/analysis/higgs/vhbb/postproc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os, sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+
+from PhysicsTools.NanoAODTools.postprocessing.analysis.higgs.vhbb.VHbbProducer import *
+files=["root://cms-xrd-global.cern.ch//store/user/arizzi/NanoTestProd006/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer17MiniAOD-92X-NanoCrabProd006/171006_144159/0000/nanolzma_1.root"]
+filesTTbar= [
+'root://cms-xrd-global.cern.ch//store/group/cmst3/group/nanoAOD/NanoTestProd006/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer17MiniAOD-92X-NanoCrabProd006/171006_155430/0000/nanolzma_1.root',
+]
+
+selection='''Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP90) >= 2  ||
+ Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP80) >= 1   ||
+ Sum$(Jet_pt > 40 && Jet_jetId) >= 4   || 
+Sum$(Jet_pt *(abs(Jet_eta)<2.5 && Jet_pt > 20 && Jet_jetId)) > 160  || 
+MET_pt > 100  || Sum$(Muon_pt > 20 && Muon_tightId) >= 1
+'''
+p=PostProcessor(".",files,selection.replace('\n',' '),"keep_and_drop.txt",[vhbb()],provenance=True)
+p.run()
