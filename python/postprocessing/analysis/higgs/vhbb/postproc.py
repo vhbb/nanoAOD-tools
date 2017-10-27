b4b491bad8423f80f92fca5a91cd67bce5a0e192
Andrea <andrea.rizzi@cern.ch>
2017-10-27 15:05:17 +0200

    initial vhbb area

---
+++ python/postprocessing/analysis/higgs/vhbb/postproc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os, sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+
+from PhysicsTools.NanoAODTools.postprocessing.analysis.higgs.vhbb.VHbbProducer import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties  import *
+files=["root://cms-xrd-global.cern.ch//store/user/arizzi/NanoTestProd006/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISummer17MiniAOD-92X-NanoCrabProd006/171006_144159/0000/nanolzma_1.root"]
+filesTTbar= [
+'root://cms-xrd-global.cern.ch//store/group/cmst3/group/nanoAOD/NanoTestProd006/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer17MiniAOD-92X-NanoCrabProd006/171006_155430/0000/nanolzma_1.root',
+]
+
+selection='''(Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP90) >= 2  ||
+ Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP80) >= 1   ||
+ Sum$(Muon_pt > 20 && Muon_tightId) >= 1 )
+ &&  Sum$((abs(Jet_eta)<2.5 && Jet_pt > 20 && Jet_jetId)) >= 2
+'''
+selectionAll='''Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP90) >= 2  ||
+ Sum$(Electron_pt > 20 && Electron_mvaSpring16GP_WP80) >= 1   ||
+ Sum$(Jet_pt > 40 && Jet_jetId) >= 4   || 
+Sum$(Jet_pt *(abs(Jet_eta)<2.5 && Jet_pt > 20 && Jet_jetId)) > 160  || 
+MET_pt > 100  || Sum$(Muon_pt > 20 && Muon_tightId) >= 1
+'''
+p=PostProcessor("/tmp/arizzi",["nanolzma_1.root"],selection.replace('\n',' '),"keep_and_drop.txt",[jecUncert_cppOut(),vhbb()],provenance=True)
+p.run()
